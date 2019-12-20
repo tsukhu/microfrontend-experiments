3f37802e3b6c5254c3e6fde8240b38bdec6fb0d3
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-12-20 16:21:04 +0530

    added css for purgecss

--- packages/header/stencil.config.ts
+++ packages/header/stencil.config.ts
@@ -3,16 +3,9 @@ import { postcss } from "@stencil/postcss";
 import autoprefixer from "autoprefixer";
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.tsx", "./src/index.html"],
+  content: ["./src/**/*.tsx","./src/**/*.css", "./src/index.html"],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
-
-//   process.env.NODE_ENV === "production" &&
-//          purgecss({
-//            content: ["./src/**/*.ts", "./src/**/*.html"],
-//            defaultExtractor: content =>
-//             content.match(/[A-Za-z0-9-_:/]+/g) || []
-//          })
 
 export const config: Config = {
   namespace: "header",
