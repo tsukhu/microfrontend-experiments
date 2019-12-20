0bdb70a6da652f1c07d8666e632a4839ae44ad63
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-12-20 16:55:53 +0530

    added tailwind menu

--- packages/header/src/components/my-component/my-component.tsx
+++ packages/header/src/components/my-component/my-component.tsx
@@ -1,9 +1,9 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, Prop, h } from "@stencil/core";
+import { format } from "../../utils/utils";
 
 @Component({
-  tag: 'my-component',
-  styleUrl: 'my-component.css',
+  tag: "my-component",
+  styleUrl: "my-component.css",
   shadow: true
 })
 export class MyComponent {
@@ -27,6 +27,63 @@ export class MyComponent {
   }
 
   render() {
-    return <h1 class="shadow-class">Hello, World! I'm {this.getText()}</h1>;
+    return (
+      <nav class="nav-root">
+        <div class="nav-logo">
+          <svg
+            class="logo-svg"
+            width="54"
+            height="54"
+            viewBox="0 0 54 54"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+          </svg>
+          <span class="nav-heading">{this.getText()}</span>
+        </div>
+        <div class="nav-sm-menu">
+          <button class="burger">
+            <svg
+              class="burger-svg"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div class="nav-items">
+          <div class="nav-item">
+            <a
+              href="#responsive-header"
+              class="nav-item-text"
+            >
+              Docs
+            </a>
+            <a
+              href="#responsive-header"
+              class="nav-item-text"
+            >
+              Examples
+            </a>
+            <a
+              href="#responsive-header"
+              class="nav-item-text"
+            >
+              Blog
+            </a>
+          </div>
+          <div>
+            <a
+              href="#"
+              class="nav-btn"
+            >
+              Download
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
